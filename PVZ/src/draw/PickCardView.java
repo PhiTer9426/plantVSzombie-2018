32d303a3d846cb690b0076a6e7e532e2046d1905
ZhaoZihao <793916584@qq.com>
2018-11-24 11:29:01 +0800

    pic

---
+++ PVZ/src/draw/PickCardView.java
@@ -0,0 +1,10 @@
+package draw;
+
+public class PickCardView {
+	private String[] picked;
+	
+	public PickCardView() {
+		// TODO Auto-generated constructor stub
+		this.picked = new String[10];
+	}
+}

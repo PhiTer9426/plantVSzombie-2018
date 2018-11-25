f62d315d587be3bac10d04f5e1a96fecce70bd5b
ZhaoZihao <793916584@qq.com>
2018-11-26 00:08:53 +0800

    merge

--- PVZ/src/draw/DrawPanel.java
+++ PVZ/src/draw/DrawPanel.java
@@ -41,51 +41,25 @@ public class DrawPanel extends JPanel{
 		}
 		if (controller.getZombies().size() != 0) {
 			for (Zombie zombie : controller.getZombies()) {
-					g.drawImage(
-						zombie.getImage(), 
-						zombie.getPosX() - zombie.getImage().getWidth(null),
-						90 + 92 + 92 * zombie.getPosY() - zombie.getImage().getHeight(null), null);
-					if((zombie.getIs_alive()==false)&&(zombie.getDeath()==0)) {
-						if(zombie.getStatus()==0) {
-					        g.drawImage(
-							    zombie.getImageOfDie(), 
-							    zombie.getDiePosX() - zombie.getImageOfDie().getWidth(null)+81,
-							    90 + 92 + 92 * zombie.getDiePosY() - zombie.getImageOfDie().getHeight(null), null);
-						}//只有第一只僵尸头会跳
-						
-						/*else if(zombie.getStatus()==1) {
-							g.drawImage(
-								    zombie.getImageOfDie(), 
-								    zombie.getDiePosX() - zombie.getImageOfDie().getWidth(null)-500,
-								    90 + 92 + 92 * zombie.getDiePosY() - zombie.getImageOfDie().getHeight(null), null);
-						}*/
-						//如果加上这一段，会卡，无法理解
+				g.drawImage(
+					zombie.getImage(), 
+					zombie.getPosX() - zombie.getImage().getWidth(null),
+					90 + 92 + 92 * zombie.getPosY() - zombie.getImage().getHeight(null), null);
+				if((zombie.getIs_alive() == false) && (zombie.getDeath() == 0)) {
+					if(zombie.getStatus() == 0) {
+				        g.drawImage(
+						    zombie.getImageOfDie(), 
+						    zombie.getDiePosX() - zombie.getImageOfDie().getWidth(null)+81,
+						    90 + 92 + 92 * zombie.getDiePosY() - zombie.getImageOfDie().getHeight(null), null);
 					}
+					else if(zombie.getStatus() == 1) {
+						g.drawImage(
+							    zombie.getImageOfDie(), 
+							    zombie.getDiePosX() - zombie.getImageOfDie().getWidth(null)-500,
+							    90 + 92 + 92 * zombie.getDiePosY() - zombie.getImageOfDie().getHeight(null), null);
+					}
+				}
 			}
 		}
-//		switch (controller.getMouse()) {
-//		case "WXZ":
-//			this.plant = new ImageIcon("plantsVsZombieMaterials/images/Plants/WXZ/WXZ.gif").getImage();
-//			g.drawImage(plant, 100, 100, null);
-//			break;
-//		case "Peashooter":
-//			this.plant = new ImageIcon("plantsVsZombieMaterials/images/Plants/Peashooter/Peashooter.gif").getImage();
-//			g.drawImage(plant, 100, 100, null);
-//			break;
-//		case "SnowPea":
-//			this.plant = new ImageIcon("plantsVsZombieMaterials/images/Plants/SnowPea/SnowPea.gif").getImage();
-//			g.drawImage(plant, 100, 100, null);
-//			break;
-//		case "WallNut":
-//			this.plant = new ImageIcon("plantsVsZombieMaterials/images/Plants/WallNut/WallNut.gif").getImage();
-//			g.drawImage(plant, 100, 100, null);
-//			break;
-//		case "TallNut":
-//			this.plant = new ImageIcon("plantsVsZombieMaterials/images/Plants/TallNut/TallNut.gif").getImage();
-//			g.drawImage(plant, 100, 100, null);
-//			break;
-//		default:
-//			break;
-//		}
 	}
 }

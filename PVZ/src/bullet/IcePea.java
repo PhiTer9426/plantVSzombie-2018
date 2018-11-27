b9fb7fa1c0d080ce74b31d967f5120538a1184ad
liushixian <a1142435775@qq.com>
2018-11-27 23:23:42 +0800

    pea go through torchwood

--- PVZ/src/bullet/IcePea.java
+++ PVZ/src/bullet/IcePea.java
@@ -3,6 +3,7 @@ package bullet;
 import javax.swing.ImageIcon;
 
 import controller.Controller;
+import plant.Plant;
 import zombie.Zombie;
 
 public class IcePea extends Bullet{
@@ -15,5 +16,29 @@ public class IcePea extends Bullet{
 	public void bulletEffect(Zombie zombie) {
 		zombie.setCurrent_health(zombie.getCurrent_health()-this.getBulletDamage());
 		zombie.setColdTime(150);
+	}
+	
+	@Override
+	public void isAttackZombie() {
+		for (Zombie zombie : this.getController().getZombies()) {
+			if ((this.getPosY()-182)/90 == zombie.getPosY() &&
+					zombie.getPosX() - this.getPosX() < 50 &&
+					zombie.getPosX() - this.getPosX() > 20) {
+				this.bulletEffect(zombie);
+				this.setRemove(true);
+				break;
+			}
+		}
+		for (Plant plant : this.getController().getPlants()) {
+			if ((this.getPosY()-182)/90 == plant.getPosY() &&
+					150 + 81 + 81 * plant.getPosX()  - this.getPosX() < 37 &&
+					150 + 81 + 81 * plant.getPosX()  - this.getPosX() > 20 &&
+					plant.getName().equals("Torchwood") ) {
+				this.setRemove(true);
+				this.getController().getBullets().add
+						(new Pea(this.getPosX() + 16, this.getPosY(), this.getController()));
+				break;
+			}
+		}
 	}
 }
